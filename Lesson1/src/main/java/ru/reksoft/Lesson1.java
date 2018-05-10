638315103ec3841bd81d311a3865342648fb180d
ya-m-a <ya-m-a@yandex.ru>
2018-05-10 12:05:08 +0300

    lesson1

--- Lesson1/src/main/java/ru/reksoft/Lesson1.java
+++ Lesson1/src/main/java/ru/reksoft/Lesson1.java
@@ -3,6 +3,7 @@ package ru.reksoft;
 public class Lesson1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Mikhail Yakotyuk!"+System.lineSeparator()+"Lesson 1 homework.");
+        System.out.println("Hello World!");
+        //System.out.println("Hello, Mikhail Yakotyuk!"+System.lineSeparator()+"Lesson 1 homework.");
     }
 }
